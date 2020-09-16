1e66cd81e9fb11df09a8659b0e42325eb618f729
Rodrigo Manriquez <rmanriquezgonzal@gmail.com>
2020-09-15 23:16:09 -0300

    docs added

---
+++ docs/precache-manifest.18e76b3da7e5e014194246d241cbf7c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909e9ff369a8c2bede56b1544c7331fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b977946f9b0092210ba1",
+    "url": "/static/css/main.63be021e.chunk.css"
+  },
+  {
+    "revision": "8c1c18475c4626965843",
+    "url": "/static/js/2.e1f41825.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e1f41825.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b977946f9b0092210ba1",
+    "url": "/static/js/main.bbb2555a.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
